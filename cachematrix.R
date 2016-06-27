2e9a1c67de7abfc0121b4ea666aca2c15ae4e1ba
carcha-git <carcha@mail.com>
2016-06-26 23:09:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mGlobal = matrix()) {
+  mInv<-NULL
+  set <- function(y) {
+    if(mGlobal!=y){
+       mGlobal <<- y
+       mInv <<- NULL
+    }
+  }
+  get <- function() mGlobal
+  setinv <- function(inv) mInv <<- inv
+  getinv <- function() mInv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mGlobal, ...) {
         ## Return a matrix that is the inverse of 'x'
+          m <- mGlobal$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  # calcula la inversa
+  data <- mGlobal$get()
+  #print(data)
+  m <- solve(data)
+  #print(m)
+  mGlobal$setinv(m)
+  m
 }
