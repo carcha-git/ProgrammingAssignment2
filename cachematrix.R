8b074fda72eddb478b77b59c5ef96c19c10f7c3e
carcha-git <carcha@mail.com>
2016-08-05 12:24:52 -0500

    Fixed
    It missed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Set the matrix in the argument as a global variable if it were not previously saved
+## mGlobal: a matrix to be saved as global 
+## NOTE: Puts the matrix inverse variable to 'null' if it was not  allready saved 
 makeCacheMatrix <- function(mGlobal = matrix()) {
   mInv<-NULL
+  # Inner function that saves the matrix in case of not being previously as a global o differs from that
   set <- function(y) {
     if(mGlobal!=y){
        mGlobal <<- y
        mInv <<- NULL
     }
   }
+  # Inner unction that returns the global matrix o null if there is no variable
   get <- function() mGlobal
   setinv <- function(inv) mInv <<- inv
   getinv <- function() mInv
@@ -19,22 +19,22 @@ makeCacheMatrix <- function(mGlobal = matrix()) {
        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of the matrix guven in the argument
+## mGlobal: list representing a matrix and its inverse as a global variable
 cacheSolve <- function(mGlobal, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          m <- mGlobal$getinv()
+  ## Return a matrix that is the inverse of 'x'
+  m <- mGlobal$getinv()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   
-  # calcula la inversa
+  # computes the inverse of the matrix
   data <- mGlobal$get()
   #print(data)
   m <- solve(data)
   #print(m)
+  # saves the inverse in the global list
   mGlobal$setinv(m)
   m
 }
